1f0e793dc3af02a3b91115128579eddf3e98c4ca
alice-lima-dev <alice.assis004@gmail.com>
2023-08-28 15:53:41 -0300

    Primeiro commit de incialização

---
+++ login kaiban/principal.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="kaiban.jpg" type="image/png">
+    <script src="js.js" defer></script>
+    
+    <title>Tela principal</title>
+</head>
+<body>
+    <nav>
+     <div class="nav">
+        <a href="#"></a>
+    </div>
+    </nav>
+</body>
+</html>
